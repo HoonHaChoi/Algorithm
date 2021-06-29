4a9b9293cc681a2b5074f99ba8f6bbd9ba2c0c88
hoonha choi <sosqsz2@naver.com>
2021-06-29 23:27:55 +0900

    이진수 변환 - 10829

--- 백준/이진수 변환 - 10829/이진수 변환 - 10829/main.swift
+++ 백준/이진수 변환 - 10829/이진수 변환 - 10829/main.swift
@@ -1,11 +1,19 @@
-//
-//  main.swift
-//  이진수 변환 - 10829
-//
-//  Created by HOONHA CHOI on 2021/06/29.
-//
-
 import Foundation
 
-print("Hello, World!")
+var arr: [Int] = []
+func binary(n: Int) {
+    if n == 0 {
+        return
+    }
+    if n == 1  {
+        arr.append(1)
+        return
+    }
+    arr.append(n%2)
+    binary(n: Int(n/2))
+}
 
+let input = Int(readLine()!)!
+binary(n: input)
+arr.reverse()
+print(arr.map { String($0) }.joined())
